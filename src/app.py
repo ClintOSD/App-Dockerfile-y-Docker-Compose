fabea26318dbf904e5cdda053bc16caf2dc42e36
ClintOSD <113477414+ClintOSD@users.noreply.github.com>
2025-07-19 17:20:53 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -19,4 +19,4 @@ def hello():
         return f"Error al conectar a la base de datos ❌: {err}"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
